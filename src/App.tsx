f9158c98455cb4a2404bd0c1e160c48256349b5f
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-22 20:29:53 +1030

    done for the day

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Box, HStack, Text} from '@chakra-ui/react'
 import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+   
+//just an exmaple, please recreate this, rember kjust create one thing at a time, but the software side first plase and maybe rediegin the thingo a bit more lol
+
+    <Box w={"500px"} h={"70px"} rounded={"xl"} boxShadow={"md"}>
+<HStack w={"100%"} h={"100% "} justify={"center"} fontWeight={"500"} fontSize={"20px"}>
+  <Text>
+  Create you goal
+  </Text>
+
+  </HStack>    
+    </Box>
+
+
+
+
+
+
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        height: "100%",
+        width: "100%",
+      },
+      body: {
+        height: "100%",
+        width: "100%",
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
+);
