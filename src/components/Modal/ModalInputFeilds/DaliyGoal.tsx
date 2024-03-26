70f9e5e2dad2a9253ff8927dbed80dad00bc7803
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-26 13:29:03 +1030

    passed onchange and value to ModalInputFeild to clean my input code, just another form of abstraction

--- src/components/Modal/ModalInputFeilds/DaliyGoal.tsx
+++ src/components/Modal/ModalInputFeilds/DaliyGoal.tsx
@@ -1,150 +1,96 @@
 import {
-    HStack,
-    VStack,
-    Text,
- 
-    Input,
-    ModalHeader,
-   
-    ModalBody,
-    Button,
-    
-  } from "@chakra-ui/react";
-import {onClose} from "../../../global/index.ts"
+  HStack,
+  VStack,
+  Text,
+  Input,
+  ModalHeader,
+  ModalBody,
+  Button,
+} from "@chakra-ui/react";
+import { onClose } from "../../../global/index.ts";
 import { Dispatch, SetStateAction, useState } from "react";
 import { FaCheck } from "react-icons/fa";
 
 import { useDispatch } from "react-redux";
 
 import { AppDispatch } from "../../../store.ts";
-import {addGoals } from "../../../features/goals/goalsSlice.tsx";
+import { addGoals } from "../../../features/goals/goalsSlice.tsx";
+import ModalInputFeild from "./ModalInputFeild.tsx";
 
+const DailyGoal: React.FC<onClose> = ({ onClose }) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [dailyGoalOne, setDailyGoalOne]: [
+    string,
+    Dispatch<SetStateAction<string>>
+  ] = useState("");
 
-const DailyGoal : React.FC<onClose> = ({onClose}) => {
-  const dispatch = useDispatch<AppDispatch>()
-    const [dailyGoalOne, setDailyGoalOne]: [string, Dispatch<SetStateAction<string>>] = useState("");
+  const [dailyGoalTwo, setDailyGoalTwo]: [
+    string,
+    Dispatch<SetStateAction<string>>
+  ] = useState("");
+  const [dailyGoalThree, setDailyGoalThree]: [
+    string,
+    Dispatch<SetStateAction<string>>
+  ] = useState("");
 
-   
-    const [dailyGoalTwo, setDailyGoalTwo]: [string, Dispatch<SetStateAction<string>>] = useState("");
-    const [dailyGoalThree, setDailyGoalThree]: [string, Dispatch<SetStateAction<string>>] = useState("");
-   
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
+    dispatch(addGoals({ DailyGoalOne: dailyGoalOne }));
+    dispatch(addGoals({ DailyGoalTwo: dailyGoalTwo }));
+    dispatch(addGoals({ DailyGoalThree: dailyGoalThree }));
+  };
 
+  return (
+    <>
+      <ModalHeader>
+        <HStack w={"100%"} h={"100%"} justify={"center"} mt={"10px"}>
+          {" "}
+          <Text fontSize={"35px"} fontWeight={"100"}>
+            {" "}
+            Set 3 Daily Goals
+          </Text>
+        </HStack>
+      </ModalHeader>
+      <ModalBody w={"100%"} h={"100%"}>
+        <VStack justify={"start"} align={"left"} w={"100%"} h={"100%"} p={2}>
+          <form onSubmit={handleSubmit}>
+            <ModalInputFeild
+              value={dailyGoalOne}
+              onChange={(e) => setDailyGoalOne(e.target.value)}
+            />
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-    
-      dispatch(addGoals({ DailyGoalOne: dailyGoalOne }));
-      dispatch(addGoals({ DailyGoalTwo: dailyGoalTwo }));
-      dispatch(addGoals({ DailyGoalThree: dailyGoalThree }));
-    
-    };
- 
-    return (
-     <>
-    
-    
-    <ModalHeader>
-            <HStack w={"100%"} h={"100%"} justify={"center"}    mt={"10px"}>
-              {" "}
-              <Text  fontSize={"35px"} fontWeight={"100"}>
-                {" "}
-                Set 3 Daily Goals
-              </Text>
+            <ModalInputFeild
+              value={dailyGoalTwo}
+              onChange={(e) => setDailyGoalTwo(e.target.value)}
+            />
+
+            <ModalInputFeild
+              value={dailyGoalThree}
+              onChange={(e) => setDailyGoalThree(e.target.value)}
+            />
+
+            <HStack w={"100%"} justify={"Center"}>
+              <Button
+                mb={"25px"}
+                colorScheme="whatsapp"
+                type="submit"
+                onClick={() => {
+                  onClose();
+
+                  console.log("im closed");
+                }}
+                w={"100%"}
+                rightIcon={<FaCheck />}
+              >
+                Done
+              </Button>
             </HStack>
-          </ModalHeader>
-          <ModalBody w={"100%"} h={"100%"}>
-            <VStack
-              justify={"start"}
-              align={"left"}
-              w={"100%"}
-              h={"100%"}
-              p={2}
-            >
-              <form onSubmit={handleSubmit}>
-            
-           
+          </form>
+        </VStack>
+      </ModalBody>{" "}
+    </>
+  );
+};
 
-                <Input
-                value={dailyGoalOne}
-                onChange={(e) => setDailyGoalOne(e.target.value)}
-                 _focus={{ border: "2px solid #B7EB8F" }}
-                 
-                  w={"100%"}
-                  
-                  fontSize={"16px"}
-                  mb={"25px"}
-                  borderRadius={"10px"}
-                  borderColor={"black"}
-       
-                  border="2px solid #EBEBEB" // Adjust the width and color as needed
-                  type="text"
-                  placeholder="I want to learn front end development"
-                />
-
-
-
-                <Input
-                value={dailyGoalTwo}
-                onChange={(e) => setDailyGoalTwo(e.target.value)}
-                  _focus={{ border: "2px solid #B7EB8F" }}
-                 
-                  w={"100%"}
-                  
-                  fontSize={"16px"}
-                  mb={"25px"}
-                  borderRadius={"10px"}
-                  borderColor={"black"}
-       
-                  border="2px solid #EBEBEB" // Adjust the width and color as needed
-                  type="text"
-                  placeholder="I want Better in REACT"
-                />
-
-
-
-                <Input
-                value={dailyGoalThree}
-                onChange={(e) => setDailyGoalThree(e.target.value)}
-                  _focus={{ border: "2px solid #B7EB8F" }}
-                 
-                  w={"100%"}
-                  
-                  fontSize={"16px"}
-                  mb={"40px"}
-                  borderRadius={"10px"}
-                  borderColor={"black"}
-       
-                  border="2px solid #EBEBEB" // Adjust the width and color as needed
-                  type="text"
-                  placeholder="I want to dabble in back end development"
-                />
-
-               
-               
-
-                <HStack w={"100%"} justify={"Center"}>
-                  <Button   mb={"25px"}
-                  
-                  colorScheme="whatsapp"
-                    type="submit"
-                    onClick={() => {
-                        onClose()
-                   
-                      console.log("im closed");
-                    
-                    }}
-                    w={"100%"}
-                    rightIcon={<FaCheck />}
-                   
-                  >
-                    Done
-                  </Button>
-                </HStack>
-              </form>
-            </VStack>
-          </ModalBody>{" "}</>
-  )
-}
-
-export default DailyGoal
+export default DailyGoal;
--- src/components/Modal/ModalInputFeilds/ModalInputFeild.tsx
+++ src/components/Modal/ModalInputFeilds/ModalInputFeild.tsx
@@ -1,12 +1,64 @@
-import React , {ReactNode} from 'react'
-
-interface childrenProps {
-    children: ReactNode; // ReactNode allows any valid JSX to be passed as children
+import {
+    HStack,
+    VStack,
+    Text,
+ 
+    Input,
+    ModalHeader,
+   
+    ModalBody,
+    Button,
+    
+  } from "@chakra-ui/react";
+interface InputFeildProps {
+    value : string 
+    onChange : (e : React.ChangeEvent<HTMLInputElement>) => void// ReactNode allows any valid JSX to be passed as children
   }
-const ModalInputFeild : React.FC<childrenProps>= ({children}) => {
+  
+
+  interface ModalTitleProps {
+    title: string
+      
+
+  }
+const ModalInputFeild : React.FC<InputFeildProps>= ({value, onChange}) => {
   return (
-    <div>ModalInputFeild</div>
+    <>
+    
+    <Input
+                value={value}
+                onChange={onChange}
+                 _focus={{ border: "2px solid #B7EB8F" }}
+                 
+                  w={"100%"}
+                  
+                  fontSize={"16px"}
+                  mb={"25px"}
+                  borderRadius={"10px"}
+                  borderColor={"black"}
+       
+                  border="2px solid #EBEBEB" // Adjust the width and color as needed
+                  type="text"
+                  placeholder="I want to learn front end development"
+                /></>
   )
 }
 
-export default ModalInputFeild
+
+
+
+const ModalTitle : React.FC<ModalTitleProps>= ({title}) => {
+    return (
+      <>
+      
+      <HStack w={"100%"} h={"100%"} justify={"center"} mt={"10px"}>
+          {" "}
+          <Text fontSize={"35px"} fontWeight={"100"}>
+            {" "}
+           {title}
+          </Text>
+        </HStack></>
+    )
+  }
+  
+  export default ModalTitle, ModalInputFeild
