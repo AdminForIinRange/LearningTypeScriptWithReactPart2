ba2c1fadc360e75718a802336260692fa71feb14
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-26 14:48:06 +1030

    cleaning up DefineYourGoals

--- src/components/Modal/ModalInputFeilds/DefineYourGoal.tsx
+++ src/components/Modal/ModalInputFeilds/DefineYourGoal.tsx
@@ -1,126 +1,93 @@
 import {
-    HStack,
-    VStack,
-    Text,
-    
-   
-    Input,
-    ModalHeader,
+  HStack,
+  VStack,
 
-    ModalBody,
-    Button,
-    FormLabel,
-  } from "@chakra-ui/react";
-  import { Dispatch, SetStateAction, useState } from "react";
-  import { FaCheck } from "react-icons/fa";
-  import {onClose} from "../../../global/index.ts"
-  import { AppDispatch } from "../../../store.ts";
-
-
-  import { useDispatch } from "react-redux";
+  Input,
+  ModalHeader,
+  ModalBody,
+  FormLabel,
+} from "@chakra-ui/react";
+import { Dispatch, SetStateAction, useState } from "react";
+import { FaCheck } from "react-icons/fa";
+import { onClose } from "../../../global/index.ts";
+import { AppDispatch } from "../../../store.ts";
+import { ModalInputFeild, ModalTitle, ModalBTN } from "./ModalInputFeild.tsx";
+import { useDispatch } from "react-redux";
 import { addGoals } from "../../../features/goals/goalsSlice.tsx";
 
+const DefineYourGoal: React.FC<onClose> = ({ onClose }) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [goalDescription, setGoalDescription]: [
+    string,
+    Dispatch<SetStateAction<string>>
+  ] = useState("");
+  const [timeEstimate, setTimeEstimate]: [
+    string,
+    Dispatch<SetStateAction<string>>
+  ] = useState("");
 
-const DefineYourGoal : React.FC<onClose> = ({onClose}) => {
-  const dispatch = useDispatch<AppDispatch>()
-    const [goalDescription, setGoalDescription]: [string, Dispatch<SetStateAction<string>>] = useState("");
-    const [timeEstimate, setTimeEstimate]: [string, Dispatch<SetStateAction<string>>] = useState("");
-  
-
-
-
+  const [formComplete, setFormComplete]: [
+    boolean,
+    Dispatch<SetStateAction<boolean>>
+  ] = useState(true);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  
+
     dispatch(addGoals({ goalDescription: goalDescription }));
- // Dispatching setGoalDescription to update goalDescription in the store
- dispatch(addGoals({ timeEstimate: timeEstimate }));
+    // Dispatching setGoalDescription to update goalDescription in the store
+    dispatch(addGoals({ timeEstimate: timeEstimate }));
   };
   return (
     <>
-    
-    
-    <ModalHeader>
-            <HStack w={"100%"} h={"100%"} justify={"center"}    mt={"10px"}>
-              {" "}
-              <Text  fontSize={"35px"} fontWeight={"100"}>
-                {" "}
-                Define Your Goal
-              </Text>
-            </HStack>
-          </ModalHeader>
-          <ModalBody w={"100%"} h={"100%"}>
-            <VStack
-              justify={"start"}
-              align={"left"}
-              w={"100%"}
-              h={"100%"}
-              p={2}
-            >
-              <form onSubmit={handleSubmit}>
-            
-              
-                <FormLabel fontWeight={400} fontSize={"17px"}>
-                  Describe your goal
-                </FormLabel>
+      <ModalHeader>
+        <ModalTitle title="Define Your Goal" />
+      </ModalHeader>
+      <ModalBody w={"100%"} h={"100%"}>
+        <VStack justify={"start"} align={"left"} w={"100%"} h={"100%"} p={2}>
+          <form onSubmit={handleSubmit}>
+            <FormLabel fontWeight={400} fontSize={"17px"}>
+              Describe your goal
+            </FormLabel>
 
-                <Input
-                value={goalDescription}
-                onChange={(e) => setGoalDescription(e.target.value)}
+            <ModalInputFeild
+              value={goalDescription}
+              onChange={(e) => setGoalDescription(e.target.value)}
+            />
+            <FormLabel fontWeight={400} fontSize={"17px"}>
+              Estimated time for achieving this goal?
+            </FormLabel>
+
+            <HStack w={"100%"} justify={"center"}>
+              <Input
+                onChange={(e) => setTimeEstimate(e.target.value)}
+                value={timeEstimate}
                 _focus={{ border: "2px solid #B7EB8F" }}
-                 
-                  w={"100%"}
-                  
-                  fontSize={"16px"}
-                  mb={"25px"}
-                  borderRadius={"10px"}
-                  borderColor={"black"}
-       
-                  border="2px solid #EBEBEB" // Adjust the width and color as needed
-                  type="text"
-                  placeholder="I want To Become a full stack web developer "
-                />
+                placeholder="Select Date"
+                size="md"
+                type="date" // Use type="date" to capture only the date
+                mb={"40px"}
+              />
+            </HStack>
 
-                <FormLabel  fontWeight={400} fontSize={"17px"}>
-                  Estimated time for achieving this goal?
-                </FormLabel>
+            <ModalBTN
+              formComplete={formComplete} //maybe use a redux state, then pass it to the ModalinputFeild
+              onClick={() => {
+                if (!timeEstimate || !goalDescription) {
+                  setFormComplete(false);
+                } else {
+                  onClose();
+                  setFormComplete(true);
 
-                <HStack w={"100%"} justify={"center"}>
-                  <Input
-                                  onChange={(e) => setTimeEstimate(e.target.value)}
-                  value={timeEstimate}
-                  _focus={{ border: "2px solid #B7EB8F" }}
-                 
-                    placeholder="Select Date"
-                    size="md"
-                    type="date" // Use type="date" to capture only the date
-                    mb={"40px"}
-                  />
-                </HStack>
+                  console.log("im closed");
+                }
+              }}
+            />
+          </form>
+        </VStack>
+      </ModalBody>{" "}
+    </>
+  );
+};
 
-                <HStack w={"100%"} justify={"Center"}>
-                  <Button   mb={"25px"}
-                  
-                  colorScheme="whatsapp"
-                    type="submit"
-                    onClick={() => {
-                        onClose()
-                   
-                      console.log("im closed");
-                    
-                    }}
-                    w={"100%"}
-                    rightIcon={<FaCheck />}
-                   
-                  >
-                    Done
-                  </Button>
-                </HStack>
-              </form>
-            </VStack>
-          </ModalBody>{" "}</>
-  )
-}
-
-export default DefineYourGoal
+export default DefineYourGoal;
