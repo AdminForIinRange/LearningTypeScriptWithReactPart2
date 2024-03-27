8d6fb3c58831560aa31848a7748b86053dfb6960
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-27 19:19:09 +1030

    Made the UI for loginsignup better

--- src/components/AuthComponents/SignUp.tsx
+++ src/components/AuthComponents/SignUp.tsx
@@ -7,16 +7,39 @@ import {
   Divider,
   Text,
   Box,
+  Progress,
 } from "@chakra-ui/react";
-import React, { Dispatch, FormEvent, FormHTMLAttributes, SetStateAction, useEffect, useState } from "react";
+import React, {
+  Dispatch,
+  FormEvent,
+  FormHTMLAttributes,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { GoogleIcon } from "../../assets/iocns/AuthIcons";
 import { AppDispatch, RootState } from "../../store.ts";
 import { useDispatch, useSelector } from "react-redux";
-import { signInWithGoogle, registerWithEmailAndPassword , sethasNotPasswordVerified, setEmailInUse, setweakPassword} from "../../features/auth/authSlice.tsx";
+import {
+  signInWithGoogle,
+  registerWithEmailAndPassword,
+  sethasNotPasswordVerified,
+  
+  setweakPassword,
+} from "../../features/auth/authSlice.tsx";
 
 const SignUp: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { isLoading, user, error, emailInUse, invalidCredential , hasNotPasswordVerified, forgotPassword, weakPassword} = useSelector((state: RootState) => state.auth);
+  const {
+    isLoading,
+   
+    errorState,
+    emailInUse,
+    
+    hasNotPasswordVerified,
+
+    weakPassword,
+  } = useSelector((state: RootState) => state.auth);
 
   const [email, setEmail]: [
     string,
@@ -46,15 +69,12 @@ const SignUp: React.FC = () => {
     }
   };
 
-
   const handleSignInWithGoogle = () => {
     dispatch(signInWithGoogle());
   };
 
-
-  if (password.length  > 5){
-    dispatch(setweakPassword(false))
-     
+  if (password.length > 5) {
+    dispatch(setweakPassword(false));
   }
 
   return (
@@ -73,22 +93,28 @@ const SignUp: React.FC = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        {emailInUse ?  <Box 
-                w={"100%"}
-                h={"30px"}
-                bg={"#FEEBC8"}
-                mt={"10px"}
-                border={" 2px dotted #F6AD55"}
-              >
-                <HStack justify={"center"}>
-                  <Text fontWeight={450} color={"#DD6B20"}>
-                    The Email you have entered is already Signed Up{" "}
-                    <Button onClick={handleSignInWithGoogle} colorScheme="yellow.100" variant="link">
-                      Login
-                    </Button>
-                  </Text>
-                </HStack>
-              </Box> : null}
+        {emailInUse ? (
+          <Box
+            w={"100%"}
+            h={"30px"}
+            bg={"#FEEBC8"}
+            mt={"10px"}
+            border={" 2px dotted #F6AD55"}
+          >
+            <HStack justify={"center"}>
+              <Text fontWeight={450} color={"#DD6B20"}>
+                The Email you have entered is already Signed Up{" "}
+                <Button
+                  onClick={handleSignInWithGoogle}
+                  colorScheme="yellow.100"
+                  variant="link"
+                >
+                  Login
+                </Button>
+              </Text>
+            </HStack>
+          </Box>
+        ) : null}
         <FormLabel mt={"20px"} htmlFor="password">
           password
         </FormLabel>
@@ -102,6 +128,23 @@ const SignUp: React.FC = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
 
+{weakPassword ? (
+          <Box
+            w={"100%"}
+            h={"30px"}
+            bg={"#FEEBC8"}
+            mt={"10px"}
+            border={" 2px dotted #F6AD55"}
+          >
+            <HStack justify={"center"}>
+              <Text fontWeight={450} color={"#DD6B20"}>
+                Password should be at least 6 characters
+              </Text>
+            </HStack>
+          </Box>
+        ) : (
+          <></>
+        )}
         <FormLabel mt={"20px"} htmlFor="password">
           Retype password
         </FormLabel>
@@ -114,6 +157,26 @@ const SignUp: React.FC = () => {
           value={rePassword}
           onChange={(e) => setRePassword(e.target.value)}
         />
+
+{hasNotPasswordVerified ? (
+          <>
+            <Box
+              w={"100%"}
+              h={"30px"}
+              bg={"#FEEBC8"}
+              mt={"10px"}
+              border={" 2px dotted #F6AD55"}
+            >
+              <HStack justify={"center"}>
+                <Text fontWeight={450} color={"#DD6B20"}>
+                  The passwords do not match
+                </Text>
+              </HStack>
+            </Box>
+          </>
+        ) : (
+          <></>
+        )}
 
         <HStack mt={"15px"} justify="space-between">
           <Checkbox>Remember me</Checkbox>
@@ -135,7 +198,12 @@ const SignUp: React.FC = () => {
           <Button colorScheme="gray" w={"20%"}>
             <GoogleIcon fontSize={"xl"} />
           </Button>
+          {isLoading && (
+            <Progress p={"0px"} m={"0px"} size="xs" isIndeterminate />
+          )}
+             <Text color="RED " fontSize={"10px"}> {errorState && errorState.message} </Text>
         </HStack>
+        
       </form>
     </>
   );
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from '../components/Navbar.tsx';
-
+import { AppDispatch, RootState } from "../store.ts";
+import { useDispatch, useSelector } from "react-redux";
 export default function RootLayout() {
+
+  
+  const {
+    user,
+  } = useSelector((state: RootState) => state.auth);
   return (
     <>
      
-     <Navbar /> 
+{  user?    <Navbar /> : null}
      
       <Outlet />
 
