74317bf68d52395b7d52ca1f0ce453998e1e007a
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-27 14:03:18 +1030

    Isolating Login and creating SignUP

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -8,47 +8,24 @@ import {
   Checkbox,
   Divider,
 } from "@chakra-ui/react";
-import { FaGoogle } from "react-icons/fa";
-import { createIcon } from "@chakra-ui/react";
 
+import { GoogleIcon } from "../../assets/iocns/AuthIcons";
 const Auth = () => {
-  const GoogleIcon = createIcon({
-    displayName: "GoogleIcon",
-    path: (
-      <g transform="matrix(1, 0, 0, 1, 27.009001, -39.238998)">
-        <path
-          fill="#4285F4"
-          d="M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z"
-        />
-        <path
-          fill="#34A853"
-          d="M -14.754 63.239 C -11.514 63.239 -8.804 62.159 -6.824 60.329 L -10.684 57.329 C -11.764 58.049 -13.134 58.489 -14.754 58.489 C -17.884 58.489 -20.534 56.379 -21.484 53.529 L -25.464 53.529 L -25.464 56.619 C -23.494 60.539 -19.444 63.239 -14.754 63.239 Z"
-        />
-        <path
-          fill="#FBBC05"
-          d="M -21.484 53.529 C -21.734 52.809 -21.864 52.039 -21.864 51.239 C -21.864 50.439 -21.724 49.669 -21.484 48.949 L -21.484 45.859 L -25.464 45.859 C -26.284 47.479 -26.754 49.299 -26.754 51.239 C -26.754 53.179 -26.284 54.999 -25.464 56.619 L -21.484 53.529 Z"
-        />
-        <path
-          fill="#EA4335"
-          d="M -14.754 43.989 C -12.984 43.989 -11.404 44.599 -10.154 45.789 L -6.734 42.369 C -8.804 40.429 -11.514 39.239 -14.754 39.239 C -19.444 39.239 -23.494 41.939 -25.464 45.859 L -21.484 48.949 C -20.534 46.099 -17.884 43.989 -14.754 43.989 Z"
-        />
-      </g>
-    ),
-  });
   return (
     <>
       <VStack w={"100%"} h={"100%"} justify={"center"} align={"center"}>
         <VStack mt={"25px"}>
           <Text fontSize={"60px"} fontWeight={"700"}>
             Login to your account
-            <Text fontSize={"30px"} fontWeight={"300"} textAlign={"center"}>
-              Dont have an Account Sign Up
+            <Text fontSize={"25px"} fontWeight={"300"} textAlign={"center"}>
+              Don't have an Account <Button variant={"link"} as={"span"}  fontWeight={"400"} fontSize={"25px"} color={"green"}> Sign Up</Button>
             </Text>
           </Text>
         </VStack>
 
         <VStack
-          p={5}
+          px={6}
+          py={3}
           mt={"25px"}
           w={"600px"}
           h={"100%"}
@@ -56,9 +33,18 @@ const Auth = () => {
           boxShadow={"0 1px 8px gray"}
           align={"left"}
         >
-          <form>
-            <FormLabel htmlFor="email"> Email</FormLabel>
-            <Input id="email" type="email" w={"100%"} required />
+          <form onSubmit={(e) => e.preventDefault()}>
+            <FormLabel mt={"20px"} htmlFor="email">
+              {" "}
+              Email
+            </FormLabel>
+            <Input
+              id="email"
+              type="email"
+              w={"100%"}
+              autoComplete="current-email"
+              required
+            />
             <FormLabel mt={"20px"} htmlFor="password">
               password
             </FormLabel>
@@ -70,9 +56,9 @@ const Auth = () => {
               w={"100%"}
             />
 
-            <HStack mt={"10px"} justify="space-between">
+            <HStack mt={"15px"} justify="space-between">
               <Checkbox>Remember me</Checkbox>
-              <Button colorScheme="whatsapp" variant="link" size="sm">
+              <Button variant="link" size="sm">
                 Forgot password?
               </Button>
             </HStack>
@@ -89,7 +75,7 @@ const Auth = () => {
               <Divider />
             </HStack>
 
-            <HStack mt={"20px"} justify={"center"} align={"center"}>
+            <HStack mt={"15px"} justify={"center"} align={"center"} mb={"20px"}>
               <Button colorScheme="gray" w={"20%"}>
                 <GoogleIcon fontSize={"xl"} />
               </Button>
